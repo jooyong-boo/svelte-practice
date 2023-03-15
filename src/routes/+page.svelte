03bced5c7dec4dbec1a27ff40a32941c813c74fc
jooyong-boo <qnwndyd159@naver.com>
2023-03-15 19:52:54 +0900

    svelte practice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,31 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+// @ts-nocheck
+
+ import Card from "../components/card.svelte";
+ import Widget from "../components/widget.svelte";
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
+<Card let:hovering>
+	<span slot="name" >
+			홍길동
+	</span>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+	<span slot="address">
+			서울특별시 여의도동
+	</span>
+	<span slot="email">
+			{#if hovering}
+					<b>myemail@google.com</b> 
+			{:else}
+					myemail@google.com
+			{/if}
+	</span>
+</Card>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<Widget>
+	<h1 slot="header">Hello</h1>
+	<svelte:fragment slot="footer">
+			<p>All rights reserved.</p>
+			<p>Copyright (c) 2019 Svelte Industries</p>
+	</svelte:fragment>
+</Widget>
